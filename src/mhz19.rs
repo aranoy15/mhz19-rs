bc73eb8bc635041ef90f88abfd327dd5205c490e
afedoseev <afedoseev@AFEDOSEEV>
2021-06-01 11:25:53 +0300

    Add comments and example of use

--- src/mhz19.rs
+++ src/mhz19.rs
@@ -29,6 +29,7 @@ pub enum AutoCalibrationState {
     Disable
 }
 
+// Calculate checksum for mhz19 packet
 fn checksum(data: &[u8]) -> u8 {
     let mut result: u8 = 0;
 
@@ -45,6 +46,7 @@ fn checksum(data: &[u8]) -> u8 {
     result
 }
 
+/// Trait for implement mhz-19 logic
 pub trait Mhz19Trait {
     type Error;
 
@@ -55,6 +57,18 @@ pub trait Mhz19Trait {
 
 const BUFFER_SIZE: usize = 9;
 
+///
+/// Mhz-19 implementation of the driver for transmission for serial.
+/// Need set serial baudrate 9600
+///
+/// # Example
+///
+/// ```
+/// let serial = Serial::new(...);
+/// let mut mhz = Mhz19::new(serial);
+///
+/// let co2: u16 = mhz.co2().unwrap();
+/// ```
 pub struct Mhz19<SerialType>
     where
         SerialType: Read<u8> + Write<u8>
@@ -75,6 +89,7 @@ impl<SerialType> Mhz19<SerialType>
         }
     }
 
+    /// Send command to mhz-19 over serial
     fn command(&mut self, cmd: u8, data: [u8; 5]) -> Result<(), Errors> {
         self.buffer = [
             0xFF,
@@ -102,6 +117,7 @@ impl<SerialType> Mhz19<SerialType>
         Ok(())
     }
 
+    /// Read response from mhz-19 driver with check checksum
     fn response(&mut self) -> Result<(), Errors> {
         for index in 0..BUFFER_SIZE {
             match nb::block!(self.serial.read()) {
@@ -126,6 +142,7 @@ impl<SerialType> Mhz19Trait for Mhz19<SerialType>
 {
     type Error = Errors;
 
+    /// Get gas concentration from mhz-19
     fn co2(&mut self) -> Result<u16, Self::Error> {
         let data: [u8; 5] = [0; 5];
 
@@ -137,6 +154,18 @@ impl<SerialType> Mhz19Trait for Mhz19<SerialType>
         Ok(result)
     }
 
+    ///
+    /// Set auto calibration or not for mhz-19 driver
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let serial = Serial::new(...);
+    /// let mut mhz = Mhz19::new(serial);
+    ///
+    /// mhz.auto_calibration(AutoCalibrationState::Disable).unwrap();
+    /// ```
+    ///
     fn auto_calibration(&mut self, state: AutoCalibrationState) -> Result<(), Self::Error> {
         let state_byte: u8 = match state {
             AutoCalibrationState::Enable => { 0xA0 }
@@ -148,6 +177,17 @@ impl<SerialType> Mhz19Trait for Mhz19<SerialType>
         self.command(Commands::AutoCalibration as u8, data)
     }
 
+    ///
+    /// Set maximum range for mhz-19 conversation (from 0 to range value)
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let serial = Serial::new(...);
+    /// let mut mhz = Mhz19::new(serial);
+    ///
+    /// mhz.range(Range::_2000).unwrap();
+    /// ```
     fn range(&mut self, range: Range) -> Result<(), Self::Error> {
         let data: [u8; 5] = match range {
             Range::_1000 => { [0x00, 0x00, 0x00, 0x03, 0xE8] }
